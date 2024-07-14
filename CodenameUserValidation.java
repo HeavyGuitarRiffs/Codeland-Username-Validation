abf04d20983a16bc7589dee2a2be6081de6d2c22
HeavyGuitarRiffs <truelee214@gmail.com>
2024-07-14 12:15:11 +0700

    Create CodenameUserValidation.java

---
+++ CodenameUserValidation.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static String CodelandUsernameValidation(String str) {
+    // code goes here  
+    return str;
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(CodelandUsernameValidation(s.nextLine())); 
+  }
+
+}
