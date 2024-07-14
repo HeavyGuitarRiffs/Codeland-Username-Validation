d1d6bcfc8eb32458acac8a79e815c7f1825e7256
HeavyGuitarRiffs <truelee214@gmail.com>
2024-07-14 13:13:16 +0700

    Update CodenameUserValidation.java

--- CodenameUserValidation.java
+++ CodenameUserValidation.java
@@ -6,6 +6,21 @@ class Main {
   public static String CodelandUsernameValidation(String str) {
     // code goes here  
     return str;
+
+       if(str.length() < 4 || str.length() > 25){
+      return "false";
+    }
+    if(!Character.toString(str.charAt(0)).matches("[a-zA-Z]")){
+      return "false";
+    }
+    if(!str.matches("^[a-zA-Z0-9_]*$")){
+      return "false";
+    }
+    if(str.charAt(str.length()-1) == '_'){
+      return "false";
+    }
+    return "true";
+  }
   }
 
   public static void main (String[] args) {  
